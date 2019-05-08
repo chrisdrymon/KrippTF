c2cacf9b9de4b3325b49badaadce747d7a4a6424
Chris Drymon <chrisdrymon@yahoo.com>
2019-05-08 04:13:18 -0500

    Expanded Trainer doesn't run well.

--- expandedTrainer.py
+++ expandedTrainer.py
@@ -6,7 +6,7 @@ import os
 import pickle
 from tensorflow.keras import layers
 
-#os.environ['CUDA_VISIBLE_DEVICES'] = ''
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
 
 df = pd.read_csv('C:\\Users\\chris\\Google Drive\\Python\\KrippHistory.csv')
 df = df.drop(columns='Bucket2')
@@ -24,7 +24,7 @@ while i < 31:
 print('Total Cards:', len(cards))
 noDups = list(set(cards))
 noDupsLen = len(noDups)
-print('Unique Cards', noDupsLen)
+print('Unique Cards:', noDupsLen)
 
 j = 0
 for card in noDups:
@@ -41,6 +41,8 @@ classDict = {'Druid': 0, 'Hunter': 1, 'Mage': 2, 'Paladin': 3, 'Priest': 4, 'Rog
 deckType = {'Aggro-Control': 0, 'Attrition': 1, 'Classic Aggro': 2, 'Classic Control': 3, 'Mid-Range': 4, 'Tempo': 5}
 expansion = {'GvG': 0, 'BRM': 1, 'WOG': 2, 'Kara': 3, 'MSG': 4, 'Ungoro': 5, 'KFT': 6, 'KnC': 7, 'Woods': 8,
              'Boomsday': 9, 'Rumble': 10, 'RoS': 11}
+
+shuffleTens = []
 
 # Create the tensors
 for row in df.itertuples():
@@ -61,30 +63,39 @@ for row in df.itertuples():
     deckScore = ((row[7] - 50) / 30)
 
     # Cards
-    cardTens = []
     k = 8
     while k < 38:
         if row[k] is not np.nan:
-            tempCT = [0] * noDupsLen
-            tempCT[cardDic[row[k]]] = 1
-            tempCT = tempCT*int(row[k+30])
-            cardTens = cardTens + tempCT
+            counter = 0
+            while counter < row[k+30]:
+                tempCT = [0] * noDupsLen
+                tempCT[cardDic[row[k]]] = 1
+                shuffleTens.append(tempCT)
+                counter += 1
         k += 1
 
-    # Combine data into a single tensor.
-    combinedTens = classTens + archTens + expanTens
-    combinedTens.append(deckScore)
-    combinedTens = combinedTens + cardTens
+    n = 0
+    while n < 1:
+        # Flatten out the lists
+        flatTens = []
+        for sublist in shuffleTens:
+            flatTens.append(sublist)
+        # Combine data into a single tensor.
+        combinedTens = classTens + archTens + expanTens
+        combinedTens.append(deckScore)
+        combinedTens = combinedTens + flatTens
 
-    # Add that tensor to the list of tensors.
-    preNump.append(combinedTens)
+        # Add that tensor to the list of tensors.
+        preNump.append(combinedTens)
 
-    # Convert labels to hots and add to the list of labels.
-    labelHot = [0, 0, 0]
-    labelHot[row[69]] = 1
-    preLabels.append(labelHot)
+        # Convert labels to hots and add to the list of labels.
+        labelHot = [0, 0, 0]
+        labelHot[row[69]] = 1
+        preLabels.append(labelHot)
+        random.shuffle(shuffleTens)
+        n += 1
 
-print('Input', len(combinedTens))
+print(len(preLabels), 'decks.')
 trainData = np.array(preNump)
 trainLabels = np.array(preLabels)
 
@@ -129,46 +140,21 @@ class ModelSaver(tf.keras.callbacks.Callback):
 modelSaver = ModelSaver()
 
 count = 0
-columnNames = ['Batch Size', 'L1 Nodes', 'L1 Regularization', 'L1 Dropout', 'L2 Nodes', 'L2 Regularization',
-               'L2 Dropout', 'Learning Rate', 'Accuracy']
 
-# oldDf = pd.read_csv('C:\\Users\\chris\\Google Drive\\Python\\hyperparams.csv')
-
-while count < 10:
-    bs1 = random.randint(1, 9)
-    bs2 = random.randint(0, 2)
-    batchSize = random.randint(1, 2000)
-    l1Nodes = random.randint(40, 1000)
-    l1Reg1 = random.randint(0, 9)
-    l1Reg2 = random.randint(-5, -1)
-    l1Reg = l1Reg1*10**l1Reg2
-    l1Dropout = random.randint(0, 80)/100
-    l2Nodes = random.randint(4, 1000)
-    l2Dropout = random.randint(0, 80)/100
-    l2Reg1 = random.randint(0, 9)
-    l2Reg2 = random.randint(-5, -1)
-    l2Reg = l2Reg1*10**l2Reg2
-    l3Nodes = random.randint(4, 1000)
-    l3Dropout = random.randint(0, 80)/100
-    l3Reg1 = random.randint(0, 9)
-    l3Reg2 = random.randint(-5, -1)
-    l3Reg = l3Reg1*10**l3Reg2
-    lr1 = random.randint(1, 9)
-    lr2 = random.randint(-6, -1)
-    lr = lr1*10**lr2
+while count < 1:
 
     tf.keras.backend.clear_session()
     graph = tf.Graph()
     with tf.Session(graph=graph):
-        model = tf.keras.Sequential([layers.Dense(l1Nodes, kernel_regularizer=tf.keras.regularizers.l1(l1Reg),
+        model = tf.keras.Sequential([layers.Dense(200, kernel_regularizer=tf.keras.regularizers.l2(0.01),
                                                   activation='relu', input_shape=(len(combinedTens),)),
-                                     layers.Dropout(l1Dropout),
-                                     layers.Dense(l2Nodes, kernel_regularizer=tf.keras.regularizers.l1(l2Reg),
+                                     layers.Dropout(.2),
+                                     layers.Dense(200, kernel_regularizer=tf.keras.regularizers.l2(0.01),
                                                   activation='relu'),
-                                     layers.Dropout(l2Dropout),
+                                     layers.Dropout(.2),
                                      layers.Dense(3, activation='softmax')])
 
-        optimizer = tf.keras.optimizers.Adam(lr=lr)
+        optimizer = tf.keras.optimizers.Adam(0.0001)
 
         model.compile(optimizer=optimizer,
                       loss='categorical_crossentropy',
@@ -177,14 +163,7 @@ while count < 10:
         callBacks = [modelSaver, tf.keras.callbacks.EarlyStopping(monitor='loss', patience=20,
                                                                   restore_best_weights=False)]
 
-        model.fit(x=trainData, y=trainLabels, batch_size=batchSize, epochs=5000, callbacks=callBacks,
+        model.fit(x=trainData, y=trainLabels, batch_size=100, epochs=5000, callbacks=callBacks,
                   validation_split=0.2, shuffle=True)
 
-#    modelSpecs = pd.DataFrame([batchSize, l1Nodes, l1Reg, l1Dropout, l2Nodes, l2Reg, l2Dropout, lr,
-#                               modelSaver.tempBest]).T
-#    modelSpecs.columns = columnNames
-#    oldDf = oldDf.append(modelSpecs, ignore_index=True, sort=False)
-
     count += 1
-
-# oldDf.to_csv('C:\\Users\\chris\\Google Drive\\Python\\hyperparams.csv', index=False)
--- testing.py
+++ testing.py
@@ -1,99 +1,29 @@
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-import os
-from tensorflow.keras import layers
+bs1 = random.randint(1, 9)
+bs2 = random.randint(0, 2)
+batchSize = random.randint(1, 2000)
+l1Nodes = random.randint(40, 1000)
+l1Reg1 = random.randint(0, 9)
+l1Reg2 = random.randint(-5, -1)
+l1Reg = l1Reg1 * 10 ** l1Reg2
+l1Dropout = random.randint(0, 80) / 100
+l2Nodes = random.randint(4, 1000)
+l2Dropout = random.randint(0, 80) / 100
+l2Reg1 = random.randint(0, 9)
+l2Reg2 = random.randint(-5, -1)
+l2Reg = l2Reg1 * 10 ** l2Reg2
+lr1 = random.randint(1, 9)
+lr2 = random.randint(-6, -1)
+lr = lr1 * 10 ** lr2
 
-#os.environ['CUDA_VISIBLE_DEVICES'] = ''
+oldDf = pd.read_csv('C:\\Users\\chris\\Google Drive\\Python\\exparams.csv')
 
-df = pd.read_csv('/home/chris/Desktop/KrippHistory.csv')
+columnNames = ['Batch Size', 'L1 Nodes', 'L1 Regularization', 'L1 Dropout', 'L2 Nodes', 'L2 Regularization',
+               'L2 Dropout', 'Learning Rate', 'Accuracy']
 
-# Create the unique card dictionary
-i = 1
-cards = []
-preNump = []
-preLabels = []
-cardDic = {}
-while i < 31:
-    cardNum = 'Card' + str(i)
-    cards = cards + df[cardNum].tolist()
-    i += 1
-print('Total Cards:', len(cards))
-noDups = list(set(cards))
-noDupsLen = len(noDups)
-print('Unique Cards', noDupsLen)
+modelSpecs = pd.DataFrame([batchSize, l1Nodes, l1Reg, l1Dropout, l2Nodes, l2Reg, l2Dropout, lr,
+                           modelSaver.tempBest]).T
+modelSpecs.columns = columnNames
+oldDf = oldDf.append(modelSpecs, ignore_index=True, sort=False)
 
-j = 0
-for card in noDups:
-    cardDic[card] = j
-    j += 1
 
-# The other dictionaries
-classDict = {'Druid': 0, 'Hunter': 1, 'Mage': 2, 'Paladin': 3, 'Priest': 4, 'Rogue': 5, 'Shaman': 6, 'Warlock': 7,
-             'Warrior': 8}
-deckType = {'Aggro-Control': 0, 'Attrition': 1, 'Classic Aggro': 2, 'Classic Control': 3, 'Mid-Range': 4, 'Tempo': 5}
-expansion = {'GvG': 0, 'BRM': 1, 'WOG': 2, 'Kara': 3, 'MSG': 4, 'Ungoro': 5, 'KFT': 6, 'KnC': 7, 'Woods': 8,
-             'Boomsday': 9, 'Rumble': 10, 'RoS': 11}
-
-# Create the tensors
-for row in df.itertuples():
-
-    # Classes
-    classTens = [0] * 9
-    classTens[classDict[row[4]]] = 1
-
-    # Archetype
-    archTens = [0] * 6
-    archTens[deckType[row[5]]] = 1
-
-    # Expansion
-    expanTens = [0] * 12
-    expanTens[expansion[row[6]]] = 1
-
-    # Normalizing the deck scores before adding them to the tensor.
-    deckScore = ((row[7] - 50) / 30)
-
-    # Cards
-    cardTens = [0] * noDupsLen
-    k = 8
-    while k < 38:
-        if row[k] is not np.nan:
-            cardTens[cardDic[row[k]]] = row[k+30]
-        k += 1
-
-    # Combine data into a single tensor.
-    combinedTens = classTens + archTens + expanTens
-    combinedTens.append(deckScore)
-    combinedTens = combinedTens + cardTens
-
-    # Add that tensor to the list of tensors.
-    preNump.append(combinedTens)
-
-    # Convert labels to hots and add to the list of labels.
-    labelHot = [0, 0, 0]
-    labelHot[row[69]] = 1
-    preLabels.append(labelHot)
-
-print('Input', len(combinedTens))
-trainData = np.array(preNump)
-trainLabels = np.array(preLabels)
-
-count = 0
-while count < 10:
-    graph = tf.Graph()
-    with tf.Session(graph=graph):
-        model = tf.keras.Sequential([layers.Dense(100, kernel_regularizer=tf.keras.regularizers.l2(.001),
-                                                  activation='relu', input_shape=(len(combinedTens),)),
-                                     layers.Dropout(0.5),
-                                     layers.Dense(100, activation='relu'),
-                                     layers.Dropout(0.3),
-                                     layers.Dense(3, activation='softmax')])
-
-        optimizer = tf.keras.optimizers.Adam(lr=0.00001)
-
-        model.compile(optimizer=optimizer,
-                      loss='categorical_crossentropy',
-                      metrics=['accuracy'])
-
-        model.fit(x=trainData, y=trainLabels, batch_size=16, epochs=20, validation_split=0.2, shuffle=True)
-    count += 1
+oldDf.to_csv('C:\\Users\\chris\\Google Drive\\Python\\hyperparams.csv', index=False)
